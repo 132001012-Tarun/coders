914bf46084a59d7f0671f0b40b288b5dccf3cfc3
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-06-06 19:15:42 +0530

    Update 3-06-23.cpp

--- 3-06-23.cpp
+++ 3-06-23.cpp
@@ -1,1 +1,34 @@
+//ADD TWO NUMBERS
 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+           ListNode*ans=new ListNode();
+            ListNode*p=ans;
+        if(l1==NULL&&l2==NULL)
+        {
+            return NULL;
+        }
+        if(!l1&&l2)
+        {
+            return l1;
+        }
+        if(l1&&!l2)
+        {
+            return l2;
+        }
+        int c=0;
+        while(l1||l2||c)
+        {
+            int sum=0;
+            sum=((l1?l1->val:0)+(l2?l2->val:0))+c;
+            c=sum/10;
+            ListNode*temp=new ListNode(sum%10);
+            p->next=temp;
+            p=p->next;
+            if(l1)l1=l1->next;
+            if(l2)l2=l2->next;
+        }
+       return ans->next;  
+    }
+};
