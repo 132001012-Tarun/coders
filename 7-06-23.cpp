427dd7cd2d0e6855e39d52d957ec1e560947145f
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-06-20 14:32:11 +0530

    Update 7-06-23.cpp

--- 7-06-23.cpp
+++ 7-06-23.cpp
@@ -1,1 +1,28 @@
-
+// K closest points to origin
+  // have used priority queue to sort it according to the greatest distance order
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,pair<int,int>>>pq;
+        vector<vector<int>>ans;
+        for(int i=0;i<points.size();i++)
+        {
+            int dist=pow(points[i][0],2)+pow(points[i][1],2);
+            pq.push({dist,{points[i][0],points[i][1]}});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        while(!pq.empty())
+        {
+            auto it=pq.top().second;
+            pq.pop();
+            vector<int>temp;
+            temp.push_back(it.first);
+            temp.push_back(it.second);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
