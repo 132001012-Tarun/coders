a915da42a67dd26ced7402685a4bc68453506ea8
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-07-11 18:15:14 +0530

    Create 23-06-23.cpp

---
+++ 23-06-23.cpp
@@ -0,0 +1,53 @@
+//BINARY TREE RIGHT SIDE VIEW
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(!root)
+        {
+            return ans;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int sz=q.size();
+            for(int i=0;i<sz;i++)
+            {
+                TreeNode*temp=q.front();
+                q.pop();
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+                if(i==sz-1)
+                {
+                    ans.push_back(temp->val);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+// ALSO YOU CAN DO LIKE AFTER EACH LEVEL YOU STORE THE NULL IN THE QUEUE IT INDICATES THAT PREVIOUS TO THE NULL IS THE LAST ELEMENT IN THIS LEVEL
