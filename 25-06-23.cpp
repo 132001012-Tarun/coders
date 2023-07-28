790e46c7caaae61017f40f2c859eab4292267e8e
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-07-28 10:40:13 +0530

    Create 25-06-23.cpp

---
+++ 25-06-23.cpp
@@ -0,0 +1,35 @@
+//Find all Anagrams in a string 
+
+// intuition is simple we must definetly have to use the sliding window concept
+// and instead of vectopr here my first intuition is maps but while comparing two maps inside the loops
+// it is showing that they are not equal even if they are equal i am still looking into the reason
+
+class Solution {
+public:
+  vector<int> findAnagrams(string s, string p) {
+        int s_len = s.length();
+        int p_len = p.length();
+        
+        if(s.size() < p.size()) return {};
+        
+        vector<int> freq_p(26,0);
+        vector<int> window(26,0);
+        
+        //first window
+        for(int i=0;i<p_len;i++){
+            freq_p[p[i]-'a']++;
+            window[s[i]-'a']++; 
+        }
+        
+        vector<int> ans;
+        if(freq_p == window) ans.push_back(0);
+        
+        for(int i=p_len;i<s_len;i++){
+            window[s[i-p_len] - 'a']--;
+            window[s[i] - 'a']++;
+            
+            if(freq_p == window) ans.push_back(i-p_len+1);
+        }
+        return ans;
+    }
+};
