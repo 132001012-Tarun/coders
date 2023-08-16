ae15dad4ca0d302cbb8d5b4ffc3b543747827dcf
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-08-16 12:18:55 +0530

    **maximum consecutive one's

---
+++ 25-07-23.cpp
@@ -0,0 +1,30 @@
+// maximum consecutive one's 
+
+// coding ninjas problem 
+
+
+int longestSubSeg(vector<int> &arr , int n, int k){
+    // Write your code here.
+    int countZ=0;
+    int mxLen=0;
+    int j=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            countZ++;
+        }
+        while (countZ > k) {
+            if(arr[j]==0)
+            {
+                countZ--;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        mxLen=max(mxLen,i-j+1);
+    }
+    return mxLen;
+}
